9e9e3bccbc37fca2ceab4e89e075649599268ecc
Vadim Vasilyev <vadim.vasilyev04@gmail.com>
2019-10-14 14:54:05 +0300

    WinTestLines

--- XO.py
+++ XO.py
@@ -3,3 +3,9 @@ if x > 2 or y > 2 or a[x][y] != 0:
     print('введите другие X и Y')
 else:
     a[x][y] = 1
+
+for line in a:
+    if not (1 in line) and not (0 in line):
+        print('бот победил')
+    if not (2 in line) and not (0 in line):
+        print('игрок победилы')

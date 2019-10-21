67c727a33b0e6d46fbe26b6edaba58c330e01267
Sigmarik <itkudryashov@gmail.com>
2019-10-21 14:19:25 +0300

    New bot as function

--- BOT.py
+++ BOT.py
@@ -1,16 +1,11 @@
 from random import randint
 
-def pront(a):
-    for i in range(len(a)):
-        print(*a[i])
-
-a = [['0', '0', '0'], ['0', '1', '0'], ['0', '0', '0']]
-#print(a)
-PosibPositions = []
-for i in range(3):
-    for j in range(3):
-        if a[i][j] == '0':
-            PosibPositions.append([i, j])
-position = PosibPositions[randint(0, len(PosibPositions))]
-a[position[0]][position[1]] = '1'
-pront(a)
+def Step(a):
+    PosibPositions = []
+    for i in range(3):
+        for j in range(3):
+            if a[i][j] == '0':
+                PosibPositions.append([i, j])
+    position = PosibPositions[randint(0, len(PosibPositions))]
+    a[position[0]][position[1]] = '1'
+    return a

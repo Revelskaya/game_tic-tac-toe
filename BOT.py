6a0ed0bfd98262e931e545f2085ebee911d2ee9a
Vadim-Vasilyev <56224150+Vadim-Vasilyev@users.noreply.github.com>
2019-10-21 14:07:57 +0300

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,16 +1,10 @@
 from random import randint
 
-def pront(a):
-    for i in range(len(a)):
-        print(*a[i])
-
-a = [['0', '0', '0'], ['0', '1', '0'], ['0', '0', '0']]
-#print(a)
+a = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 PosibPositions = []
 for i in range(3):
     for j in range(3):
-        if a[i][j] == '0':
+        if a[i][j] == 0:
             PosibPositions.append([i, j])
 position = PosibPositions[randint(0, len(PosibPositions))]
-a[position[0]][position[1]] = '1'
-pront(a)
+a[position[0]][position[1]] = 2
